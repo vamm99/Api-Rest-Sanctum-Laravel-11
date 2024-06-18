1b82c99e5aa4cfd7f015166b556eb7c9b8b01eca
Victor Mejia <130195315+vamm99@users.noreply.github.com>
2024-06-18 09:34:26 -0500

    add: Update Route

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,20 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('register',[AuthController::class, 'register']);
+Route::post('/register',[AuthController::class, 'register']);
 
-Route::post('login',[AuthController::class, 'login']);
+Route::post('/login',[AuthController::class, 'login']);
 
-Route::apiResource('products',ProductController::class)->middleware('auth:sanctum');
+// Protejemos las rutas 
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    // products
+    Route::apiResource('/products',ProductController::class);
+
+    //logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// Route::apiResource('products',ProductController::class)->middleware('auth:sanctum');
+
+// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
